a98f333a361901f13eb4a57401cdcef78388380d
Shahzad Sayyed <fa429728@gmail.com>
2023-06-15 13:59:10 +0530

    Add functions

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def sum(a,b):
-    return a+b;
+    return a+b
+
+def diff(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+
+
 
 num1=10
 num2=20
 print(sum(num1,num2))
+print(diff(num1,num2))
+print(mul(num1,num2))
